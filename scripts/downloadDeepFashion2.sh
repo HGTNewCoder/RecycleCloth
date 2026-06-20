ee14bcb0d7ebcf05b0a6b63dfb857afda3342953
HGTNewCoder <thinh.hagia22@gmail.com>
2026-06-20 06:47:25 +0000

    Update

--- scripts/downloadDeepFashion2.sh
+++ scripts/downloadDeepFashion2.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-curl -L -o /home/ubuntu/iai2o/downloadedZip/deepfashion2-original-with-dataframes.zip\
+set -e  # stop if any command fails
+
+ZIP_DIR="/home/ubuntu/iai2o/downloadedZip"
+ZIP_FILE="$ZIP_DIR/deepfashion2-original-with-dataframes.zip"
+DATASET_DIR="/home/ubuntu/iai2o/Datasets"
+
+# Create directories if they don't exist
+mkdir -p "$ZIP_DIR"
+mkdir -p "$DATASET_DIR"
+
+# Download dataset
+curl -L -o "$ZIP_FILE" \
   https://www.kaggle.com/api/v1/datasets/download/thusharanair/deepfashion2-original-with-dataframes
+
+# Unzip after download
+unzip -o "$ZIP_FILE" -d "$DATASET_DIR"
+``
--- scripts/unzipDeepFashion2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-unzip -o /home/ubuntu/iai2o/downloadedZip/deepfashion2-original-with-dataframes.zip -d /home/ubuntu/iai2o/Datasets
