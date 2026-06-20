a2409c2ca3b755cc32ae71a466dd15218336e2db
HGTNewCoder <thinh.hagia22@gmail.com>
2026-06-20 06:34:42 +0000

    Initial commit

---
+++ scripts/downloadDeepFashion2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o /home/ubuntu/iai2o/downloadedZip/deepfashion2-original-with-dataframes.zip\
+  https://www.kaggle.com/api/v1/datasets/download/thusharanair/deepfashion2-original-with-dataframes
---
+++ scripts/unzipDeepFashion2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+unzip -o /home/ubuntu/iai2o/downloadedZip/deepfashion2-original-with-dataframes.zip -d /home/ubuntu/iai2o/Datasets
